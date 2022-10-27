aa018d7914d7efb7135d534667f9ffd48bc8be12
AirSupan <airsupansa23@gmail.com>
2022-10-27 16:35:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data(){
+        return {
+            message: "Hello World!",
+            fname: "Supansa",
+            count: 0,
+        }
+    }
+
+}).mount("#app")
